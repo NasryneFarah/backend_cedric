418282d2418cec42d26e2c3e0e54322e482fc7a5
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2025-09-09 12:39:39 +0000

    ajout fichier env

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const hostname = '127.0.0.1';
 
 // connexion base de donnee
 const mysql = require("mysql")
+ const env = require('dotenv').config();
 const connexion = mysql.createConnection({
-  host : '127.0.0.1',
-  user : "root",
-  password : "",
-  database : 'dashboard_nuxt',
+  host : process.env.DB_HOST,
+  user : process.env. DB_USER,
+  password : process.env. DB_PASSWORD,
+  database : process.env. DB_NAME,
 })
 
 connexion.connect((err) => {
